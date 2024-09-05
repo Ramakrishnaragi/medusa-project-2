c5fbaee89064f418332112ed8ff68a319b170674
Dhiraj-Shivade <dhiraj.shivade1@gmail.com>
2024-09-06 00:04:15 +0530

    Added DB-Subnet-Group

--- rds.tf
+++ rds.tf
@@ -1,5 +1,14 @@
+resource "aws_db_subnet_group" "medusa_db_subnet_group" {
+  name       = "medusa-db-subnet-group"
+  subnet_ids = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+  tags = {
+    Name = "medusa-db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "medusa_db" {
   allocated_storage    = 20
+  storage_type            = "gp2"
   engine               = "postgres"
   engine_version       = "16.4"
   instance_class       = "db.t3.micro"
@@ -7,5 +16,6 @@ resource "aws_db_instance" "medusa_db" {
   username             = var.db_username
   password             = var.db_password
   vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  db_subnet_group_name    = aws_db_subnet_group.medusa_db_subnet_group.name
   skip_final_snapshot  = true
 }
